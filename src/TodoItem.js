b1c8c2ede2bdf7bbed525490681ffe10bd7a9bbb
balalaXD <liferec@163.com>
2018-08-19 19:15:43 +0800

    Add Component props types check and default props when not specified

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
 function RenderTodoItems({items, onClick}) {
   const itemsList = items.map((item, index) => {
@@ -12,6 +13,17 @@ function RenderTodoItems({items, onClick}) {
       {itemsList}
     </ul>
   )
+}
+
+RenderTodoItems.propTypes = {
+  items: propTypes.array.isRequired,
+  onClick: propTypes.func.isRequired
+}
+
+// When you not provide some props to the component, the default take effect
+// For more detail, visit https://reactjs.org/docs/typechecking-with-proptypes.html
+RenderTodoItems.defaultProps = {
+  items: ['Wake up at 5:30', 'Working hard until 7:00pm', 'Go to study']
 }
 
 export default RenderTodoItems;
