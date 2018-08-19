7031d3d933eba1f607acb372f94e92bb34e70f81
balalaXD <liferec@163.com>
2018-08-19 13:19:21 +0800

    Add todoList

---
+++ src/todoList.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      inputValue: 'ab',
+      todoItems: []
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.handleItemDelete = this.handleItemDelete.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+
+    this.setState({
+      inputValue: target.value
+    })
+  }
+
+  handleButtonClick(event) {
+    this.setState({
+      inputValue: '',
+      todoItems: [...this.state.todoItems, this.state.inputValue]
+    })
+  }
+
+  handleItemDelete(index) {
+    const todoItemsCopy = [...this.state.todoItems];
+    todoItemsCopy.splice(index, 1);
+
+    this.setState({
+      todoItems: todoItemsCopy
+    })
+  }
+
+  render() {
+    function RenderTodoItems({items, onClick}) {
+      const itemsList = items.map((item, index) => {
+        return (
+          <li key={index} onClick={() => onClick(index)}>{item}</li>
+        )
+      })
+
+      return (
+        <ul>
+          {itemsList}
+        </ul>
+      )
+    }
+
+    return (
+      <React.Fragment>
+        <div>
+          <input type="text"
+            value={this.state.inputValue}
+            onChange={this.handleInputChange}/>
+          <button onClick={this.handleButtonClick}>Add</button>
+        </div>
+        <RenderTodoItems items={this.state.todoItems} onClick={(index) => this.handleItemDelete(index)} />
+      </React.Fragment>
+    );
+  }
+}
+
+export default TodoList;
