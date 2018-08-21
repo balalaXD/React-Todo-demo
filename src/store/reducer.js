e080f690190938ace37412a25f52626350a67517
balalaXD <liferec@163.com>
2018-08-21 21:52:59 +0800

    Add some comment about reducer definition

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -9,6 +9,11 @@ function deepCopy(json) {
   return JSON.parse(JSON.stringify(json));
 }
 
+// reducer function must be pure function, the result will not related with time, ajax request etc.
+// that's given one input, return same output regardless of time and others
+//
+// newState.inputValue = new Date()   // WRONG
+// state.inputValue = action.value    // WRONG, you can't change immutable object
 export default (prevState = defaultState, action) => {
   if (action.type === ACTIONTYPES.CHANGE_INPUT_VALUE) {
     let newState = deepCopy(prevState)
