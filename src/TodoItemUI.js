ea56a89c9c528490d263cf09207daa6989816060
balalaXD <liferec@163.com>
2018-08-21 22:16:50 +0800

    Refactor UI Component to function stateless Component

--- src/TodoItemUI.js
+++ src/TodoItemUI.js
@@ -3,33 +3,31 @@ import { Button, Input,
          Row, Col, Container } from 'reactstrap';
 import RenderTodoItems from './TodoItem';
 
-class TodoItemUI extends Component {
-  render () {
-    return (
-      <React.Fragment>
-        <Container>
-          <Row>
-            <Col md={{ size: 8 }}>
-              <Input type="text" placeholder="Add your Todo here"
-                value={this.props.inputValue}
-                onChange={this.props.handleInputChange} />
-            </Col>
-            <Col md={{size: 2, offset: 0}}>
-              <Button 
-                onClick={this.props.handleButtonClick}
-                color="primary"
-                style={{width: '100%'}}>Add</Button>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <RenderTodoItems items={this.props.todoItems} onClick={(index) => this.props.handleItemDelete(index)} />
-            </Col>
-          </Row>
-        </Container>
-      </React.Fragment>
-    )
-  }
+const TodoItemUI = (props) => {
+  return (
+    <React.Fragment>
+      <Container>
+        <Row>
+          <Col md={{ size: 8 }}>
+            <Input type="text" placeholder="Add your Todo here"
+              value={props.inputValue}
+              onChange={props.handleInputChange} />
+          </Col>
+          <Col md={{size: 2, offset: 0}}>
+            <Button 
+              onClick={props.handleButtonClick}
+              color="primary"
+              style={{width: '100%'}}>Add</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <RenderTodoItems items={props.todoItems} onClick={(index) => props.handleItemDelete(index)} />
+          </Col>
+        </Row>
+      </Container>
+    </React.Fragment>
+  )
 }
 
 export default TodoItemUI;
